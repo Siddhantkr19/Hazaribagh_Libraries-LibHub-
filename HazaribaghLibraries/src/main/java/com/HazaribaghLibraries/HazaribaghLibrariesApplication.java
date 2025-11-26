58628ecb7edb33976c00809adfe9d28a73e5feb6
siddh <siddhantkumar7488@gmail.com>
2025-11-26 10:24:48 +0530

    first commit

---
+++ HazaribaghLibraries/src/main/java/com/HazaribaghLibraries/HazaribaghLibrariesApplication.java
@@ -0,0 +1,15 @@
+package com.HazaribaghLibraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazaribaghLibrariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazaribaghLibrariesApplication.class, args);
+
+		System.out.println("hey i am back ");
+	}
+
+}
