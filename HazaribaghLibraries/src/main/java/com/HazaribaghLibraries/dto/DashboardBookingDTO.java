a7385ab892c61047a45bfeae8feeb6533affb7cc
siddh <siddhantkumar7488@gmail.com>
2025-11-28 16:41:30 +0530

    Creating a DashBoardBooking.java : what ever i  want to show the user i map field  with  this.

---
+++ HazaribaghLibraries/src/main/java/com/HazaribaghLibraries/dto/DashboardBookingDTO.java
@@ -0,0 +1,29 @@
+package com.HazaribaghLibraries.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class DashboardBookingDTO {
+
+    // Booking Identity
+    private Long bookingId;
+    private String status; // CONFIRMED or EXPIRED
+    private Double amountPaid; // e.g. 350.0
+
+    // Seat Info
+    private String seatNumber; // e.g. "B4"
+
+    // Library Info (So the student knows where to go)
+    private String libraryName;
+    private String libraryAddress;
+    private String libraryOwnerContact;
+
+    // Time Calculations
+    private LocalDateTime bookingDate;
+    private LocalDateTime validUntil; // Expiry Date
+
+    // The "Red Alert" Counter
+    private long daysRemaining; // e.g. 29 (Calculated in Service)
+}
