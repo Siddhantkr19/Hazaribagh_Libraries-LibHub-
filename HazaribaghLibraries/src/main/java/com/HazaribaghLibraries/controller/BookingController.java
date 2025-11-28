89414c586283b245de0d9c3a889ab704d5ef85f4
siddh <siddhantkumar7488@gmail.com>
2025-11-28 18:51:07 +0530

    Add @Get metthod user can see there history for specific library.

--- HazaribaghLibraries/src/main/java/com/HazaribaghLibraries/controller/BookingController.java
+++ HazaribaghLibraries/src/main/java/com/HazaribaghLibraries/controller/BookingController.java
@@ -2,15 +2,15 @@ package com.HazaribaghLibraries.controller;
 
 import com.HazaribaghLibraries.dto.BookingRequestDTO;
 import com.HazaribaghLibraries.dto.DashboardBookingDTO;
+import com.HazaribaghLibraries.dto.PaymentHistoryDTO;
 import com.HazaribaghLibraries.entity.Booking;
 import com.HazaribaghLibraries.service.BookingService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/bookings")
 @CrossOrigin(origins = "http://localhost:3000")
 public class BookingController {
@@ -18,7 +18,6 @@ public class BookingController {
 
     public BookingController(BookingService bookingService) {
         this.bookingService = bookingService;
-
     }
 
 
@@ -35,5 +34,13 @@ public class BookingController {
         return  ResponseEntity.ok(bookingService.getUserDashboard(userEmail));
 
     }
+    // 3. Get Payment History
+    // URL: GET http://localhost:8080/api/bookings/history?userEmail=rahul@gmail.com&libraryId=1
+    @GetMapping("/history")
+    public ResponseEntity<List<PaymentHistoryDTO>> getHistory(
+            @RequestParam String userEmail,
+            @RequestParam Long libraryId) {
 
+        return ResponseEntity.ok(bookingService.getPaymentHistory(userEmail, libraryId));
+    }
 }
