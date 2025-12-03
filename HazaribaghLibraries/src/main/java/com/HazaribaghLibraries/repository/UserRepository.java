28829094ad4e9c444f791c3f6f04a6097b384053
siddh <siddhantkumar7488@gmail.com>
2025-12-03 16:48:50 +0530

    update

--- HazaribaghLibraries/src/main/java/com/HazaribaghLibraries/repository/UserRepository.java
+++ HazaribaghLibraries/src/main/java/com/HazaribaghLibraries/repository/UserRepository.java
@@ -10,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email); // cheak email is already exist or not.
+
+    String email(String email);
 }
