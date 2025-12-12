6eae988ef6be2bd7b20c55fce580d2777a61d8fa
siddh <siddhantkumar7488@gmail.com>
2025-12-12 17:08:03 +0530

    connect it to the frontend url.

--- HazaribaghLibraries/src/main/java/com/HazaribaghLibraries/controller/LibraryController.java
+++ HazaribaghLibraries/src/main/java/com/HazaribaghLibraries/controller/LibraryController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 
 @RequestMapping("/api/libraries") // All URLs start with this
-@CrossOrigin(origins = "http://localhost:3000") // Allows React to talk to Java
+@CrossOrigin(origins = "http://localhost:5173") // Allows React to talk to Java
 public class LibraryController {
 
     private final LibraryService libraryService;
